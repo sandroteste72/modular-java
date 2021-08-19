9228009e072cac0bfcd081d239a67897dab3e9e8
sandroteste72 <sandro.teste72@gmail.com>
2021-08-19 03:05:02 -0300

    Cleaning project

--- digital.innovation.one.core/src/digital/innvation/one/core/Runner.java
+++ digital.innovation.one.core/src/digital/innvation/one/core/Runner.java
@@ -1,7 +1,10 @@
 package digital.innvation.one.core;
 
+import digital.innovation.one.utils.operation.Calculator;
+
 public class Runner {
     public static void main(String[] args) {
-
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(5, 9));
     }
 }
